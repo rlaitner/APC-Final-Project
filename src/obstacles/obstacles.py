de5b82b5835741b000382d4c90f6a67a1e2d2123
lhlam3168 <79770376+lhlam3168@users.noreply.github.com>
2021-12-07 17:13:06 -0500

    Misunderstood the assignment, replaced code w/ the 3 classes

--- src/obstacles/obstacles.py
+++ src/obstacles/obstacles.py
@@ -1,29 +1,23 @@
-import json
-import numpy as np
-import matplotlib.pyplot as plt
-from typing import List, Tuple
-from scipy.spatial import distance
+class circleObstacle():
+    
+    # Class for circle obstacles
+    def __init__(self, center, radius, cost):
+        self.center = vertices      # Center of circle object
+        self.radius = radius        # Radius of circle object
+        self.cost = cost            # Cost associated with the circle object
 
-%matplotlib notebook
+class quadrilateralObstacle():
 
-width = 
-height = 
-circleObstacles = [(np.array([1, 1]), 0.25), (np.array([2, 2]), 0.25), (np.array([1, 3]), 0.3)]
-quadrilateralObstacles = [(np.array([3, 4]), 4, 5), (np.array([6, 3]), 2, 1), (np.array([2, 2]), 1, 1)]
-triangleObstacles = [(np.array([3, 4]), np.array([4, 4]), np.array([5, 5])), (np.array([8, 2]), np.array([1, 0]), np.array([2, 2]))]
+    # Class for quadrilateral obstacles
+    def __init__(self, origin, height, width, cost):
+        self.origin = origin        # Starting point of quadrilateral of which height and width are referenced to
+        self.height = height        # Height of quadrilateral object
+        self.width = width          # Width of quadrilateral object
+        self.cost = cost            # Cost associated with the quadrilateral object
 
-%matplotlib inline
-fig, ax = plt.subplots()
+class triangleObstacle():
 
-ax.set_xlim([0, width])
-ax.set_ylim([0, height])
-ax.set_aspect('equal')
-
-for c in circleObstacles:
-    ax.add_artist(plt.Circle(tuple(c[0]), c[1]))
-
-for q in quadrilateralObstacles:
-    ax.add_artist(plt.Rectangle(tuple(q[0]), q[1], q[2]))
-
-for t in triangleObstacles:
-    ax.add_artist(plt.Polygon(tuple(t[0]), tuple(t[1]), tuple(t[2])))
+    # Class for triangle obstacles
+    def __init__(self, vertices, cost):
+        self.vertices = vertices    # List of vertices that belong to the triangle object
+        self.cost = cost            # Cost associated with the triangle object

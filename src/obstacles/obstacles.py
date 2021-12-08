b2b9ce786c13fcfba64bd02e9aa93cc7d0b7e7bf
lhlam3168 <79770376+lhlam3168@users.noreply.github.com>
2021-12-08 18:08:43 -0500

    Changed name from quadrilateral to rectangle

--- src/obstacles/obstacles.py
+++ src/obstacles/obstacles.py
@@ -6,14 +6,14 @@ class circleObstacle():
         self.radius = radius        # Radius of circle object
         self.cost = cost            # Cost associated with the circle object
 
-class quadrilateralObstacle():
+class rectangleObstacle():
 
-    # Class for quadrilateral obstacles
+    # Class for rectangle obstacles
     def __init__(self, origin, height, width, cost):
-        self.origin = origin        # Starting point of quadrilateral of which height and width are referenced to
-        self.height = height        # Height of quadrilateral object
-        self.width = width          # Width of quadrilateral object
-        self.cost = cost            # Cost associated with the quadrilateral object
+        self.origin = origin        # Starting point of rectangle of which height and width are referenced to
+        self.height = height        # Height of rectangle object
+        self.width = width          # Width of rectangle object
+        self.cost = cost            # Cost associated with the rectangle object
 
 class triangleObstacle():
 

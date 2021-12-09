5a92389a29bd715ea1f9046c26e605b1475fdf34
lhlam3168 <79770376+lhlam3168@users.noreply.github.com>
2021-12-08 19:43:38 -0500

    Forgot to mention renamed height to length for rectangle obstacles and removed cost for triangle because I missed it last time

--- src/obstacles/obstacles.py
+++ src/obstacles/obstacles.py
@@ -2,22 +2,19 @@ class circleObstacle():
     
     # Class for circle obstacles
     def __init__(self, center, radius, cost):
-        self.center = center      # Center of circle object
+        self.center = center        # Center of circle object
         self.radius = radius        # Radius of circle object
-        self.cost = cost            # Cost associated with the circle object
 
 class rectangleObstacle():
 
     # Class for rectangle obstacles
-    def __init__(self, origin, height, width, cost):
+    def __init__(self, origin, length, width, cost):
         self.origin = origin        # Starting point of rectangle of which height and width are referenced to
-        self.height = height        # Height of rectangle object
+        self.length = length        # Length of rectangle object
         self.width = width          # Width of rectangle object
-        self.cost = cost            # Cost associated with the rectangle object
 
 class triangleObstacle():
 
     # Class for triangle obstacles
     def __init__(self, vertices, cost):
         self.vertices = vertices    # List of vertices that belong to the triangle object
-        self.cost = cost            # Cost associated with the triangle object

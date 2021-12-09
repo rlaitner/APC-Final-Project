25d27d50d3c8022a8d9b236957f6340157e4a324
rlaitner <30702201+rlaitner@users.noreply.github.com>
2021-12-08 21:46:13 -0500

    Developed the constructor for the agent class

--- src/pathingSim/agent.py
+++ src/pathingSim/agent.py
@@ -6,10 +6,31 @@ Classes
 -------
 Agent
 """
+from typing import Dict, Union
+
 from pathingSim.pathing_algorithm import PathingAlgorithm
+from pathingSim.vehicle import Vehicle
+from pathingSim.vehicle_factory import vehicle_factory
 from pathingSim.algo_factory import algo_factory
 
 class Agent():
-    def __init__(self, algo: str) -> None:
-        self._planner: PathingAlgorithm = algo_factory(algo)
-        self._vehicle = None
+    def __init__(self,
+                 vehicle_data: Dict[str, Union[str, float, list]],
+                 algo_data: Dict[str, Union[str, float, list]]
+                 ) -> None:
+
+        # Instantiate objects
+        self._planner: PathingAlgorithm = algo_factory(algo_data["algorithm_type"])
+        self._vehicle: Vehicle = vehicle_factory(vehicle_data["vehicle_type"])
+
+        # Configure objects
+        self._planner.set_config(algo_data)
+        self._vehicle.set_config(vehicle_data)
+
+        self._trajectory = None
+
+    def is_valid(self):
+        pass
+
+    def get_valid_trajectory(self):
+        pass

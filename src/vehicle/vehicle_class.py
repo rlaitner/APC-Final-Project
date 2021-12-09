c250ae895aba32901d0d19e4fe31cc96d7fb51d1
Kevin Andrade <kevin.andrade@princeton.edu>
2021-12-09 01:20:20 -0500

    updated dynamics

--- src/vehicle/vehicle_class.py
+++ src/vehicle/vehicle_class.py
@@ -19,6 +19,7 @@ class Vehicle:
     def car_dynamics(x_curr, y_curr, theta_curr):
         u = 1
         L = 1
+        W = 1
         x_dot = u * np.cos(theta)
         y_dot = u * np.sin(theta)
         theta_dot = u/L * tan(phi)
@@ -33,27 +34,35 @@ class Vehicle:
         psi = 45 - theta_curr
         nu = 180 - 45 - theta_curr
 
-        d = 1
-        
-        dx1 = d * np.sin(psi)
-        dy1 = d * np.cos(psi)
+        d = calc_d(L,W) 
+        car_position = shape_coords(d, psi, nu, position)
 
-        dx2 = d * np.sin(nu)
-        dy2 = d * np.cos(nu)
-
-        top_left = [position[0]+ dx2, position[1] + dy2]
-        bottom_right = [position[0]- dx2, position[1] - dy2]
- 
-        top_right = [position[0]+ dx1, position[1] + dy1]
-        bottom_left = [position[0]- dx1, position[1] - dy1]       
-    
-        car_position = [top_left, top_right, bottom_left, bottom_right]
         return car_position 
 
     def UAV_dynamics(x_curr, y_curr, theta_curr, t):
 
     
-        return position 
+
+    def calc_d(L, W):
+        return (np.sqrt((L/2)**2 + (H/2)**2))
+    def calc_dx(d, angle):
+        return (d  * np.sin(theta))
+    def calc_dy(d, angle):
+        return (d  * np.cos(theta))  
+    def shape_coords(d, psi, nu, position):
+        dx1 = calc_dx(d, psi)
+        dy1 = calc_dy(d, psi)
+        dx2 = calc_dx(d, nu)
+        dy2 = calc_dy(d, nu)
+
+        top_left = [position[0] + dx2, position[1] + dy2]
+        bottom_right = [position[0]- dx2, position[1] - dy2]
+ 
+        top_right = [position[0] + dx1, position[1] + dy1]
+        bottom_left = [position[0] - dx1, position[1] - dy1]     
+
+        vehicle_coords = [top_left, top_right, bottom_left, bottom_right]
+        return vehicle_coords
 
 
 

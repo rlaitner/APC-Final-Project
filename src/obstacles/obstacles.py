0abffff43bfd39a0241125f5161e404711ac6ca8
lhlam3168 <79770376+lhlam3168@users.noreply.github.com>
2021-12-11 15:03:28 -0500

    Added lines attribute

--- src/obstacles/obstacles.py
+++ src/obstacles/obstacles.py
@@ -2,19 +2,39 @@ class circleObstacle():
     
     # Class for circle obstacles
     def __init__(self, center, radius):
-        self.center = center        # Center of circle object
-        self.radius = radius        # Radius of circle object
+        # Center of circle object
+        self.center = center        
+        
+        # Radius of circle object
+        self.radius = radius
 
 class rectangleObstacle():
 
     # Class for rectangle obstacles
     def __init__(self, origin, length, width):
-        self.origin = origin        # Starting point of rectangle of which length and width are referenced to
-        self.length = length        # Length of rectangle object
-        self.width = width          # Width of rectangle object
-
+        # Starting point of rectangle of which length and width are referenced to
+        self.origin = origin
+        
+        # Length of rectangle object
+        self.length = length
+        
+        # Width of rectangle object
+        self.width = width
+        
+        # Lines that form the rectangle
+        self.lines = [[(origin[0], origin[1]), (origin[0] + width, origin[1])], 
+                      [(origin[0], origin[1]), (origin[0], origin[1] + length)],
+                      [(origin[0], origin[1] + length), (origin[0] + width, origin[1] + length)],
+                      [(origin[0] + width, origin[1]), (origin[1] + width, origin[1] + length)]]
+        
 class triangleObstacle():
 
     # Class for triangle obstacles
     def __init__(self, vertices):
-        self.vertices = vertices    # List of vertices that belong to the triangle object
+        # List of vertices that belong to the triangle object
+        self.vertices = vertices
+        
+        # Lines that form the triangle
+        self.lines = [[(vertices[0][0], vertices[0][1]), (vertices[1][0], vertices[1][1])],
+                      [(vertices[0][0], vertices[0][1]), (vertices[2][0], vertices[2][1])],
+                      [(vertices[1][0], vertices[1][1]), (vertices[2][0], vertices[2][1])]]

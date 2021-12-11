c75989412fa99fca46b5f68c9f0553b6eb3f6498
Kevin Andrade <kevin.andrade@princeton.edu>
2021-12-11 16:42:18 -0500

    seperated helper function for calculating rectangle corners

--- src/vehicle/vehicle_class_whelper.py
+++ src/vehicle/vehicle_class_whelper.py
@@ -28,28 +28,10 @@ class Vehicle:
     Methods: 
     ----------------------
     
-    update_gvehicle():
-        Updates the position of the ground vehicle given its dynamics 
-        
-        returns:
-            List of 
-            
-    
-    update_UAV():
-        Updates the position of the Unmanned Aerial Vehicle given its dynamics 
-        
-    set_vehicle_size():
-        Sets the appropriate dimensions of the vehicle given the scaling factor 
-        
-    update_dynamics():
-        Updates vehicle dynamics given vehicle type and control input
-    
-    
-    
 
     ''' 
 
-    def __init__(self, vehicle_type, scale, x_init, y_init, theta_init):
+    def __init__(self, vehicle_type, scale, x_init, y_init, angle):
         if (vehicle_type == "car"):
             self.type = "car"
             self.shape = "rectangle"
@@ -60,24 +42,15 @@ class Vehicle:
             self.type = "UAV" 
             self.shape = "circle"
             
-        self.size = self.set_vehicle_size(scale)
+        self.scale = scale
+        self.size = self.set_vehicle_size()
         self.x_init = x_init
         self.y_init = y_init 
-        self.theta_init = theta_init
-        
-        
-            
-    def update_dynamics(self, x, y, theta, turn_angle):
-        if (self.type == "car"):
-            return self.update_gvehicle(x, y, theta, turn_angle)
-        elif (self.type == "tricycle"):
-            return self.update_gvehicle(x, y, theta, turn_angle)
-        elif (self.type == "UAV"):
-            return self.update_UAV(x, y)
+        self.angle = angle
         
     
     def update_gvehicle(self, x, y, theta, turn_angle, u=1, dt=0.1):
-        
+
         if (np.abs(turn_angle) >= np.pi/2):
             raise ValueError("Turn angle exceed maximum turn radius\
                             for vehicle of type 'car'")        
@@ -92,34 +65,63 @@ class Vehicle:
         y += y_dot * dt
         theta += theta_dot * dt 
         
-        gvehicle_position = [x, y, theta]        
+        center_position = [x, y]        
+
+        d = (np.sqrt((L/2)**2 + (W/2)**2))
+        gvehicle_position = self.shape_coords(d, theta, center_position)
+        gvehicle_position.insert(0, theta)
+        gvehicle_position.insert(0, center_position)
 
         return gvehicle_position
     
 
-    def update_UAV(self, x, y, dt=0.1):
+    def update_UAV(self, x, y, theta, turn_angle, dt=0.1):
         x_dot = 1
         y_dot = 1
         
         x += x_dot * dt
         y += y_dot * dt
         
-        UAV_position = [x, y]
+        UAV_position = [x, y, self.size]
         
         return UAV_position 
 
 
-    def set_vehicle_size(self, scale):
+    def set_vehicle_size(self):
         if (self.type == "car"):     
-            L = 2 * scale
-            W = 1.5 * scale
+            L = 2 * self.scale
+            W = 1.5 * self.scale
             return (L,W)
         elif (self.type == "tricycle"):
-            L = 2 * scale
-            W = 0.5 * scale   
+            L = 2 * self.scale
+            W = 0.5 * self.scale   
             return (L,W)
         elif (self.type == "UAV"):
-            R = 0.5 * scale
+            R = 0.5 * self.scale
             return R
+        
+
+    def update_dynamics(self, x, y, theta, turn_angle):
+        if (self.type == "car"):
+            return self.update_gvehicle(x, y, theta, turn_angle)
+        elif (self.type == "tricycle"):
+            return self.update_gvehicle(x, y, theta, turn_angle)
+        elif (self.type == "UAV"):
+            return self.update_UAV(x, y, theta, turn_angle)
     
     
+    def shape_coords(self, d, theta, position):
+        psi = np.pi/4 - theta
+        nu = np.pi - np.pi/4 - theta
+        dx1 = d * np.sin(psi)
+        dy1 = d * np.cos(psi)
+        dx2 = d * np.sin(nu)
+        dy2 = d * np.cos(nu)
+        
+        top_left = [position[0] + dx2, position[1] + dy2]
+        bottom_right = [position[0] - dx2, position[1] - dy2]
+        top_right = [position[0] + dx1, position[1] + dy1]
+        bottom_left = [position[0] - dx1, position[1] - dy1]     
+
+        gvehicle_coords = [top_left, top_right, bottom_left, bottom_right]
+        return gvehicle_coords 

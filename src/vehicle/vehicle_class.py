9f0ca3df4c67903584e436ea92e36bc3c3cb31c0
Kevin Andrade <kevin.andrade@princeton.edu>
2021-12-10 20:19:14 -0500

    commented and cleaned code

--- src/vehicle/vehicle_class.py
+++ src/vehicle/vehicle_class.py
@@ -1,91 +1,97 @@
+'''
+Create vehicle for path planning. 
+
+Classes:
+    Vehicle
+
+'''
 import numpy as np
 
 
 class Vehicle: 
 
-    def __init__(self, vehicle_type):
-        
+    '''
+    Create a vehicle object 
+
+    Attributes:
+    -----------------------
+
+    x : double 
+        Position of COM along the x-axis on the map in cm
+
+    y : double 
+        Position of COM along the y-axis on the map in cm
+
+    theta : double 
+        Angle between x-axis and centerline of vehicle in radians 
+
+
+    ''' 
+
+    def __init__(self, vehicle_type, scale):
         if (vehicle_type == "car"):
             self.type = "car"
         elif (vehicle_type == "UAV"):
             self.type = "UAV" 
         elif (vehicle_type == "tricycle"):
             self.type = "tricycle"
+        self.scale = scale
         
         
-    def update_car(self, x_curr, y_curr, theta_curr, phi):
-        u = 1.5
-        L = 2
-        W = 1
-        theta_dot = u/L * np.tan(phi)
-        x_dot = u * np.cos(theta_curr)
-        y_dot = u * np.sin(theta_curr)
+    def update_vehicle(self, x, y, theta, turn_angle):
+
+        if (self.type == "car"):     
+            L = 2 * self.scale
+            W = 1 * self.scale
+            if (np.abs(turn_angle) >= np.pi/2):
+                raise ValueError("Turn angle exceed maximum turn radius for vehicle of type 'car'")
+        elif (self.type == "tricycle"):
+            L = 2 * self.scale
+            W = 0.5 * self.scale            
         
-        t = 0.1
-        x_curr += x_dot * t
-        y_curr += y_dot * t
-        theta_curr += theta_dot * t 
+        u = 1
+        theta_dot = u/L * np.tan(turn_angle)
+        x_dot = u * np.cos(theta)
+        y_dot = u * np.sin(theta)
         
-        center_position = [x_curr, y_curr]        
-        psi = np.pi/4 - theta_curr
-        nu = np.pi - np.pi/4 - theta_curr
+        dt = 0.1
+        x += x_dot * dt
+        y += y_dot * dt
+        theta += theta_dot * dt 
+        
+        center_position = [x, y]        
+        psi = np.pi/4 - theta
+        nu = np.pi - np.pi/4 - theta
 
         d = self.calc_d(L,W) 
         car_position = self.shape_coords(d, psi, nu, center_position)
-        car_position.insert(0, theta_curr)
+        car_position.insert(0, theta)
         car_position.insert(0, center_position)
 
         return car_position
     
-    def update_tricycle(self, x_curr, y_curr, theta_curr, phi):
-        u = 1.5
-        L = 3
-        W = 0.5
-        theta_dot = u/L * np.tan(phi)
-        x_dot = u * np.cos(theta_curr)
-        y_dot = u * np.sin(theta_curr)
-        
-        t = 0.1
-        x_curr += x_dot * t
-        y_curr += y_dot * t
-        theta_curr += theta_dot * t 
-        
-        center_position = [x_curr, y_curr]        
-        psi = np.pi/4 - theta_curr
-        nu = np.pi - np.pi/4 - theta_curr
 
-        d = self.calc_d(L,W) 
-        car_position = self.shape_coords(d, psi, nu, center_position)
-        car_position.insert(0, theta_curr)
-        car_position.insert(0, center_position)
-
-        return car_position
-
-
-    def update_UAV(self, x_curr, y_curr, theta_curr):
-        
+    def update_UAV(self, x, y, theta, turn_angle):
         x_dot = 1
         y_dot = 1
         R = 1
 
-        t = 1
-        x_curr += x_dot * t
-        y_curr += y_dot * t
-        
-        position = np.array([x_curr, y_curr, 0])
-        UAV_position = [x_curr, y_curr, R]
+        dt = 0.1
+        x += x_dot * dt
+        y += y_dot * dt
+        UAV_position = [x, y, R]
 
         return UAV_position 
 
 
-    def update_dynamics(self, x_curr, y_curr, theta_curr):
+    def update_dynamics(self, x, y, theta, turn_angle):
         if (self.type == "car"):
-            return self.update_car(x_curr, y_curr, theta_curr)
-        elif (self.type == "UAV"):
-            return self.update_UAV(x_curr, y_curr, theta_curr)
+            return self.update_vehicle(x, y, theta, turn_angle)
         elif (self.type == "tricycle"):
-            return self.update_tricycle(x_curr, y_curr, theta_curr)
-        
+            return self.update_vehicle(x, y, theta, turn_angle)
+        elif (self.type == "UAV"):
+            return self.update_UAV(x, y, theta, turn_angle)
+
 
     def calc_d(self, L, W): return (np.sqrt((L/2)**2 + (W/2)**2))
     def calc_dx(self, d, angle): return (d  * np.sin(angle))

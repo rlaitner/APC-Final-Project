fe588cfbf617810af0235aa563088b84b1773081
Kevin Andrade <kevin.andrade@princeton.edu>
2021-12-11 14:26:56 -0500

    added initial positions to  vehicles

--- src/vehicle/vehicle_class.py
+++ src/vehicle/vehicle_class.py
@@ -15,7 +15,6 @@ class Vehicle:
 
     Attributes:
     -----------------------
-
     x : double 
         Position of COM along the x-axis on the map in cm
 
@@ -24,11 +23,15 @@ class Vehicle:
 
     theta : double 
         Angle between x-axis and centerline of vehicle in radians 
-
+        
+    
+    Methods: 
+    ----------------------
+    
 
     ''' 
 
-    def __init__(self, vehicle_type, scale):
+    def __init__(self, vehicle_type, scale, x_init, y_init, angle):
         if (vehicle_type == "car"):
             self.type = "car"
         elif (vehicle_type == "UAV"):
@@ -36,77 +39,85 @@ class Vehicle:
         elif (vehicle_type == "tricycle"):
             self.type = "tricycle"
         self.scale = scale
+        self.size = self.set_vehicle_size()
+        self.x_init = x_init
+        self.y_init = y_init 
+        self.angle = angle
         
-        
-    def update_vehicle(self, x, y, theta, turn_angle):
+    
+    def update_gvehicle(self, x, y, theta, turn_angle, u=1, dt=0.1):
 
-        if (self.type == "car"):     
-            L = 2 * self.scale
-            W = 1 * self.scale
-            if (np.abs(turn_angle) >= np.pi/2):
-                raise ValueError("Turn angle exceed maximum turn radius for vehicle of type 'car'")
-        elif (self.type == "tricycle"):
-            L = 2 * self.scale
-            W = 0.5 * self.scale            
+        if (np.abs(turn_angle) >= np.pi/2):
+            raise ValueError("Turn angle exceed maximum turn radius\
+                            for vehicle of type 'car'")        
+        L = self.size[0]
+        W = self.size[1]
         
-        u = 1
         theta_dot = u/L * np.tan(turn_angle)
         x_dot = u * np.cos(theta)
         y_dot = u * np.sin(theta)
         
-        dt = 0.1
         x += x_dot * dt
         y += y_dot * dt
         theta += theta_dot * dt 
         
         center_position = [x, y]        
-        psi = np.pi/4 - theta
-        nu = np.pi - np.pi/4 - theta
 
-        d = self.calc_d(L,W) 
-        car_position = self.shape_coords(d, psi, nu, center_position)
-        car_position.insert(0, theta)
-        car_position.insert(0, center_position)
+        d = (np.sqrt((L/2)**2 + (W/2)**2))
+        gvehicle_position = self.shape_coords(d, theta, center_position)
+        gvehicle_position.insert(0, theta)
+        gvehicle_position.insert(0, center_position)
 
-        return car_position
+        return gvehicle_position
     
 
-    def update_UAV(self, x, y, theta, turn_angle):
+    def update_UAV(self, x, y, theta, turn_angle, dt=0.1):
         x_dot = 1
         y_dot = 1
-        R = 1
-
-        dt = 0.1
+        
         x += x_dot * dt
         y += y_dot * dt
-        UAV_position = [x, y, R]
-
+        
+        UAV_position = [x, y, self.size]
+        
         return UAV_position 
 
 
+    def set_vehicle_size(self):
+        if (self.type == "car"):     
+            L = 2 * self.scale
+            W = 1.5 * self.scale
+            return (L,W)
+        elif (self.type == "tricycle"):
+            L = 2 * self.scale
+            W = 0.5 * self.scale   
+            return (L,W)
+        elif (self.type == "UAV"):
+            R = 0.5 * self.scale
+            return R
+        
+
     def update_dynamics(self, x, y, theta, turn_angle):
         if (self.type == "car"):
-            return self.update_vehicle(x, y, theta, turn_angle)
+            return self.update_gvehicle(x, y, theta, turn_angle)
         elif (self.type == "tricycle"):
-            return self.update_vehicle(x, y, theta, turn_angle)
+            return self.update_gvehicle(x, y, theta, turn_angle)
         elif (self.type == "UAV"):
             return self.update_UAV(x, y, theta, turn_angle)
-
-
-    def calc_d(self, L, W): return (np.sqrt((L/2)**2 + (W/2)**2))
-    def calc_dx(self, d, angle): return (d  * np.sin(angle))
-    def calc_dy(self, d, angle): return (d  * np.cos(angle))  
-    def shape_coords(self, d, psi, nu, position):
-        dx1 = self.calc_dx(d, psi)
-        dy1 = self.calc_dy(d, psi)
-
-        dx2 = self.calc_dx(d, nu)
-        dy2 = self.calc_dy(d, nu)
+    
+    
+    def shape_coords(self, d, theta, position):
+        psi = np.pi/4 - theta
+        nu = np.pi - np.pi/4 - theta
+        dx1 = d * np.sin(psi)
+        dy1 = d * np.cos(psi)
+        dx2 = d * np.sin(nu)
+        dy2 = d * np.cos(nu)
         
         top_left = [position[0] + dx2, position[1] + dy2]
-        bottom_right = [position[0]- dx2, position[1] - dy2]
+        bottom_right = [position[0] - dx2, position[1] - dy2]
         top_right = [position[0] + dx1, position[1] + dy1]
         bottom_left = [position[0] - dx1, position[1] - dy1]     
 
-        vehicle_coords = [top_left, top_right, bottom_left, bottom_right]
-        return vehicle_coords 
+        gvehicle_coords = [top_left, top_right, bottom_left, bottom_right]
+        return gvehicle_coords 

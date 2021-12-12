31225babe1d72566d3984d24bda8ffc50b6d9ba0
Kevin Andrade <kevin.andrade@princeton.edu>
2021-12-12 16:11:11 -0500

    added render_environment functionallity

--- src/visualizer_class/visualizer.py
+++ src/visualizer_class/visualizer.py
@@ -1,68 +1,34 @@
-import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
+from matplotlib import patches as patches
 
-
-
-uav = UAV("UAV", (0,0), 1)
-
-
-sequence = []
-U = [1,1]
-position = uav.update_dynamics(0,0, U)
-sequence.append(position)
-for i in range(0, 10):
-    position = uav.update_dynamics(position[0], position[1], U)
-    sequence.append(position)
+class Visualizer():
     
-U = [0,0.5]
-for i in range(0, 10):
-    position = uav.update_dynamics(position[0], position[1], U)
-    sequence.append(position)    
-
-    
-U = [1,0.3]
-for i in range(0, 10):
-    position = uav.update_dynamics(position[0], position[1], U)
-    sequence.append(position) 
-
-
-    
-fig, ax = plt.subplots()
-
-ax.set_xlim(0,20)
-ax.set_ylim(0,20)
-plt.plot(x,y, c="black")
-drone = plt.Circle((1, 1), 1, fc='black')
-
-
-x_line = np.zeros(len(sequence))
-y_line = np.zeros(len(sequence))
-
-for i in range(0, len(sequence)):
-    x_line[i] = sequence[i][0]
-    y_line[i] = sequence[i][1]
-
-
-
-
-def init():
-    drone.center = (1,1)
-    ax.add_patch(drone)
-    return drone
-
-def animate(i):
-    x, y = drone.center
-    x = sequence[i][0]
-    y = sequence[i][1]
-    drone.center = (x, y)
-    return drone
-
-anim = animation.FuncAnimation(fig, animate, 
-                               init_func=init, 
-                               frames=1000, 
-                               interval=200,
-                               blit=True)
-
-plt.show()
-
+    def __init__(self, dims, obstacles, vehicle):
+        (self.x, self.y) = dims
+        self.obstacles = obstacles
+        #self.obstacles = environment.obstacles 
+        self.vehicle_shape = vehicle.type
+        
+    def render_environment(self):
+        fig,ax = plt.subplots()  
+        ax.set_xlim(0,self.x)
+        ax.set_ylim(0,self.y)
+        
+        for obstacle in self.obstacles:
+            if (obstacle.shape == "rectangle"):
+                x = obstacle.position[0]
+                y = obstacle.position[1]
+                rect = patches.Rectangle((x, y), obstacle.width, obstacle.length)
+                ax.add_patch(rect)
+            elif (obstacle.shape == "circle"):
+                x = obstacle.position[0]
+                y = obstacle.position[1]
+                circ = patches.Circle((x, y), obstacle.radius)
+                ax.add_patch(circ)  
+            elif (obstacle.shape == "triangle"):
+                xy1 = obstacle.position[0]
+                xy2 = obstacle.position[1]
+                xy3 = obstacle.position[2]
+                triang = patches.Polygon((xy1, xy2, xy3))
+                ax.add_patch(triang) 

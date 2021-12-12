35646174d1f8c555edeb83ea12d23dce277a596c
rlaitner <30702201+rlaitner@users.noreply.github.com>
2021-12-11 20:07:16 -0500

    Flaked vehicle class

--- src/vehicle/helper_rect_coord.py
+++ src/vehicle/helper_rect_coord.py
@@ -1,9 +1,9 @@
 import numpy as np
 
+
 def rect_coords(d, theta, position):
     psi = np.pi/4 - theta
     nu = np.pi - np.pi/4 - theta
-
 
     dx1 = d * np.sin(psi)
     dy1 = d * np.cos(psi)
@@ -13,7 +13,7 @@ def rect_coords(d, theta, position):
     top_left = [position[0] + dx2, position[1] + dy2]
     bottom_right = [position[0] - dx2, position[1] - dy2]
     top_right = [position[0] + dx1, position[1] + dy1]
-    bottom_left = [position[0] - dx1, position[1] - dy1]     
+    bottom_left = [position[0] - dx1, position[1] - dy1]
 
     gvehicle_coords = [top_left, top_right, bottom_left, bottom_right]
-    return gvehicle_coords 
+    return gvehicle_coords

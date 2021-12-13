dc73776945b8e6c5b1ece9e6dfe4254024526415
Kevin Andrade <kevin.andrade@princeton.edu>
2021-12-12 21:57:35 -0500

    cleaned render_vehicle function

--- src/vehicle_class/tricycle.py
+++ src/vehicle_class/tricycle.py
@@ -30,26 +30,16 @@ class Tricycle(Vehicle):
         return tricycle_position     
     
     def render_vehicle(self, position):
-        def rect_coords(d, position):
+    
+        def calc_origin(d, position):
             psi = np.pi/4 - position[2]
-            nu = np.pi - np.pi/4 - position[2]
-
-            dx1 = d * np.sin(psi)
-            dy1 = d * np.cos(psi)
-            dx2 = d * np.sin(nu)
-            dy2 = d * np.cos(nu)
-
-            top_left = [position[0] + dx2, position[1] + dy2]
-            bottom_right = [position[0] - dx2, position[1] - dy2]
-            top_right = [position[0] + dx1, position[1] + dy1]
-            bottom_left = [position[0] - dx1, position[1] - dy1]
-
-            gvehicle_coords = [top_left, top_right, bottom_left, bottom_right]
-            return gvehicle_coords
-        
+            dx = d * np.sin(psi)
+            dy = d * np.cos(psi)
+            bottom_left = [position[0] - dx, position[1] - dy]
+            return bottom_left
         
         d = np.sqrt((self.size[0]**2) + (self.size[1]**2))
-        x,y = rect_coords(d, position)[2]
+        x,y = calc_origin(d, position)
         tricycle = patches.Rectangle((x, y), self.size[1], self.size[0],
                                    angle = np.degrees(position[2]))  
         return tricycle

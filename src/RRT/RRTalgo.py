d1297d8d592de23ae65418bd42d06360c162dad0
lhlam3168 <79770376+lhlam3168@users.noreply.github.com>
2021-12-12 21:55:06 -0500

    Added edge_free function along w/ other functions it needs

--- src/RRT/RRTalgo.py
+++ src/RRT/RRTalgo.py
@@ -81,3 +81,117 @@ def free_conf(q: Tuple[np.ndarray, float], obstacles: List[Tuple[np.ndarray, flo
                 return False
         
     return True
+
+        # Given three collinear points p, q, r, the function checks if
+    # point q lies on line segment 'pr'
+    def onSegment(p, q, r):
+        if ( (q.x <= max(p.x, r.x)) and (q.x >= min(p.x, r.x)) and
+               (q.y <= max(p.y, r.y)) and (q.y >= min(p.y, r.y))):
+            return True
+        return False
+
+    def orientation(p, q, r):
+        # To find the orientation of an ordered triplet (p,q,r)
+        # function returns the following values:
+        # 0 : Collinear points
+        # 1 : Clockwise points
+        # 2 : Counterclockwise
+     
+        val = (float(q.y - p.y) * (r.x - q.x)) - (float(q.x - p.x) * (r.y - q.y))
+        if (val > 0):
+         
+            # Clockwise orientation
+            return 1
+        elif (val < 0):
+         
+            # Counterclockwise orientation
+            return 2
+        else:
+         
+            # Collinear orientation
+            return 0
+
+    # The main function that returns true if
+    # the line segment 'p1q1' and 'p2q2' intersect.
+    def doIntersect(p1,q1,p2,q2):
+     
+        # Find the 4 orientations required for
+        # the general and special cases
+        o1 = orientation(p1, q1, p2)
+        o2 = orientation(p1, q1, q2)
+        o3 = orientation(p2, q2, p1)
+        o4 = orientation(p2, q2, q1)
+ 
+        # General case
+        if ((o1 != o2) and (o3 != o4)):
+            return True   
+        
+        # Special Cases
+ 
+        # p1 , q1 and p2 are collinear and p2 lies on segment p1q1
+        if ((o1 == 0) and onSegment(p1, p2, q1)):
+            return True
+ 
+        # p1 , q1 and q2 are collinear and q2 lies on segment p1q1
+        if ((o2 == 0) and onSegment(p1, q2, q1)):
+            return True
+ 
+        # p2 , q2 and p1 are collinear and p1 lies on segment p2q2
+        if ((o3 == 0) and onSegment(p2, p1, q2)):
+            return True
+ 
+        # p2 , q2 and q1 are collinear and q1 lies on segment p2q2
+        if ((o4 == 0) and onSegment(p2, q1, q2)):
+            return True
+ 
+        # If none of the cases
+        return False
+    
+    def edge_free(edge: Tuple[np.ndarray, np.ndarray], obstacles: List[Tuple[np.ndarray, float]]) -> bool:
+    """
+    Check if a graph edge is in the free space.
+    
+    This function checks if a graph edge, i.e. a line segment specified as two end points, lies entirely outside of
+    every obstacle in the configuration space.
+    
+    @param edge: A tuple containing the two segment endpoints.
+    @param obstacles: A list of obstacles as described.
+    @return: True if the edge is in the free space, i.e. it lies entirely outside of all the obstacles in `obstacles`. 
+             Otherwise return False.
+    """
+    
+    o = edge[0]
+    f = edge[1]
+    
+    u = (f-o)
+    u_norm = u/np.linalg.norm(u)
+    
+    for i in range(0, len(obstacles)):
+        curr_obstacle = obstacles[i]
+        
+        # If the obstacle is a circle
+        if len(curr_obstacle) == 2:
+            
+            # Store circle center (x,y) value 
+            c = curr_obstacle[0]    
+            # Store circle radius 
+            r = curr_obstacle[1]
+        
+            # Solve for nabla
+            nabla = (np.dot(u_norm, (o - c)))**2 - (np.dot(o - c, o - c) - r ** 2)
+        
+            if (nabla >= 0):
+                return False
+        
+        # Obstacle is either a rectangle or a triangle
+        else:
+            
+            # Iterates through all lines of each polygon 
+            for k in obstacles[i].lines:
+                new_p2 = obstacles[i].lines[k][0]
+                new_q2 = obstacles[i].lines[k][1]
+                
+                if doIntersect(o, f, new_p2, new_q2):
+                    return False
+        
+    return True

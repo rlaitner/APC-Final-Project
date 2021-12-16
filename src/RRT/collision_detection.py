5f646149dbd3a356f14efbe40e3e239571dd328a
lhlam3168 <79770376+lhlam3168@users.noreply.github.com>
2021-12-15 19:35:14 -0500

    Added collision_detection

---
+++ src/RRT/collision_detection.py
@@ -0,0 +1,86 @@
+def edges_of(vertices):
+    """
+    Return the vectors for the edges of the polygon p.
+    p is a polygon.
+    """
+    edges = []
+    N = len(vertices)
+
+    for i in range(N):
+        edge = vertices[(i + 1)%N] - vertices[i]
+        edges.append(edge)
+
+    return edges
+
+def orthogonal(v):
+    """
+    Return a 90 degree clockwise rotation of the vector v.
+    """
+    return np.array([-v[1], v[0]])
+
+def is_separating_axis(o, p1, p2):
+    """
+    Return True and the push vector if o is a separating axis of p1 and p2.
+    Otherwise, return False and None.
+    """
+    min1, max1 = float('+inf'), float('-inf')
+    min2, max2 = float('+inf'), float('-inf')
+
+    for v in p1:
+        projection = np.dot(v, o)
+
+        min1 = min(min1, projection)
+        max1 = max(max1, projection)
+
+    for v in p2:
+        projection = np.dot(v, o)
+
+        min2 = min(min2, projection)
+        max2 = max(max2, projection)
+
+    if max1 >= min2 and max2 >= min1:
+        d = min(max2 - min1, max1 - min2)
+        # Push a bit more than needed so the shapes do not overlap in future
+        # tests due to float precision
+        d_over_o_squared = d/np.dot(o, o) + 1e-10
+        return False
+    else:
+        return True
+
+def centers_displacement(p1, p2):
+    """
+    Return the displacement between the geometric center of p1 and p2.
+    """
+    # Geometric center
+    c1 = np.mean(np.array(p1), axis=0)
+    c2 = np.mean(np.array(p2), axis=0)
+    return c2 - c1
+    
+
+def polygonCollide(p1, p2):
+    '''
+    Return True and the MPV if the shapes collide. Otherwise, return False and
+    None.
+
+    p1 and p2 are lists of ordered pairs, the vertices of the polygons in the
+    counterclockwise direction.
+    '''
+
+    p1 = [np.array(v, 'float64') for v in p1]
+    p2 = [np.array(v, 'float64') for v in p2]
+
+    edges = edges_of(p1)
+    edges += edges_of(p2)
+    orthogonals = [orthogonal(e) for e in edges]
+
+    push_vectors = []
+    for o in orthogonals:
+        separates, pv = is_separating_axis(o, p1, p2)
+
+        if separates:
+            # They do not collide and there is no push vector
+            return False, None
+        else:
+            push_vectors.append(pv)
+
+    return True

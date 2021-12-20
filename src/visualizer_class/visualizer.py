a2a8f030870d5172873652983146da76f1e17af9
Kevin Andrade <kevin.andrade@princeton.edu>
2021-12-20 00:05:06 -0500

    added path graph

--- src/visualizer_class/visualizer.py
+++ src/visualizer_class/visualizer.py
@@ -7,6 +7,7 @@ from vehicle import Vehicle, Car, UAV, Tricycle
 import numpy as np
 
 
+
 class Visualizer():
     def __init__(self, dims, obstacles, vehicle):
         (self.x, self.y) = dims
@@ -14,21 +15,23 @@ class Visualizer():
         #self.obstacles = environment.obstacles 
         self.vehicle_shape = vehicle.type
         self.vehicle = vehicle
+        self.goal = (13,13)
         
     def render_environment(self):
         fig, ax = plt.subplots()
         ax.set_xlim(0,self.x)
         ax.set_ylim(0,self.y)
-        
         for obstacle in self.obstacles:
             ax.add_patch(obstacle.render_obstacle())
+        
+        ax.add_patch(patches.Rectangle(self.goal, 2, 2, color="green"))
         return (fig, ax) 
     
     
     def render_path(self, path, fig, ax):
         
         def start():
-            vehicle_shape.center = (vehicle.x_init, vehicle.y_init)
+            vehicle_shape.center = (self.vehicle.x_init, self.vehicle.y_init)
             ax.add_patch(vehicle_shape)
             ax.patches.pop()
             return vehicle_shape,
@@ -39,11 +42,16 @@ class Visualizer():
             ax.add_patch(vehicle_shape)
             return vehicle_shape,
         
-        
+        line = np.zeros((len(path), 2))
+        for i in range(0, len(path)):
+            line[i][0] = path[i][0]
+            line[i][1] = path[i][1]
+        plt.plot(line[:,0], line[:,1], color="black")
+        plt.grid(True)
         vehicle_shape = self.vehicle.render_vehicle(path[0])
         ax.add_patch(vehicle_shape)
         anim = animation.FuncAnimation(fig, animate,
                                frames=1000, init_func = start,
-                               interval=50, repeat = True,
+                               interval=200, repeat = True,
                                blit=True)
         return anim
